3aa3c06392fe4ce3edc7259915aad8f452369105
Zeyneppsevgi <zeyneppsevgi@gmail.com>
2024-01-31 20:44:55 +0300

    kpss puan hesaplama

---
+++ KPSSPuanHesaplama/Resources/KPSSPuanHesaplamaApp.swift
@@ -0,0 +1,28 @@
+//
+//  KPSSPuanHesaplamaApp.swift
+//  KPSSPuanHesaplama
+//
+//  Created by Zeynep Sevgi on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct KPSSPuanHesaplamaApp: App {
+    init() {
+        let appearanceNav = UINavigationBarAppearance()
+        appearanceNav.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = appearanceNav
+        UINavigationBar.appearance().scrollEdgeAppearance = appearanceNav
+        
+        let appearanceTab = UITabBarAppearance()
+        appearanceTab.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = appearanceTab
+        UITabBar.appearance().scrollEdgeAppearance = appearanceTab
+    }
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ KPSSPuanHesaplama/Views/RootView.swift
@@ -0,0 +1,35 @@
+//
+//  RootView.swift
+//  KPSSPuanHesaplama
+//
+//  Created by Zeynep Sevgi on 31.01.2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @State private var selectionItem = 0
+    var body: some View {
+        TabView(selection: $selectionItem)
+                 {
+            MainView()
+                         .tabItem {
+                             Label("Başlangıç", systemImage: "house")
+                                 .environment(\.symbolVariants, selectionItem == 0 ? .fill: .none)
+                         }
+                         .tag(0)//selectıonıtem ı burdan takip edecek
+                     ResultView()
+                         .tabItem {
+                             Label("Hesaplamalar", systemImage: "arrow.counterclockwise.circle")
+                                 .environment(\.symbolVariants, selectionItem == 1 ? .fill: .none)
+                         }
+                         .tag(1)
+        }
+                 .tint(.orange)
+    }
+        
+}
+
+#Preview {
+    RootView()
+}
