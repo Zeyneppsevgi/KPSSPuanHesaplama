f6b487eae42368cd9833827831808c911e2308e2
Zeyneppsevgi <zeyneppsevgi@gmail.com>
2024-01-31 20:43:51 +0300

    KPSS puan hesaplama
    KPSS puan hesaplama With SWiftUI

---
+++ KPSSPuanHesaplama/Views/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  KPSSPuanHesaplama
+//
+//  Created by Zeynep Sevgi on 31.01.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            .navigationTitle("KPSS Puan Hesaplama")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ KPSSPuanHesaplama/Views/ResultView.swift
@@ -0,0 +1,23 @@
+//
+//  ResultView.swift
+//  KPSSPuanHesaplama
+//
+//  Created by Zeynep Sevgi on 31.01.2024.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            .navigationTitle("Geçmiş Hesaplamalar")
+        }
+    }
+}
+
+#Preview {
+    ResultView()
+}
